e46eb35c20541c75910f29014bf8bb07ce087f94
Eldho Abe <eldhoabe.07@gmail.com>
2016-02-18 19:25:45 +0530

    - Implemented timing finding mechanism
    - Need to check the last time in the session

--- Factorypattern/Program.cs
+++ Factorypattern/Program.cs
@@ -11,21 +11,43 @@ namespace Factorypattern
         static void Main(string[] args)
         {
             var ls = new List<Programms>();
-            ls.Add(new Programms { Duration = 30, Title = "Welcome session" });
-            ls.Add(new Programms { Duration = 30, Title = "Welcome session" });
-            ls.Add(new Programms { Duration = 60, Title = "Welcome session" });
-            ls.Add(new Programms { Duration = 45, Title = "Welcome session" });
+            ls.Add(new Programms { Duration = 30, Title = "Welcome session 1" });
+            ls.Add(new Programms { Duration = 30, Title = "Welcome session 2" });
+            ls.Add(new Programms { Duration = 60, Title = "Welcome session 3" });
+            ls.Add(new Programms { Duration = 45, Title = "Welcome session 4" });
 
-            ls.Add(new Programms { Duration = 30, Title = "Welcome session" });
-            ls.Add(new Programms { Duration = 15, Title = "Welcome session" });
-            ls.Add(new Programms { Duration = 60, Title = "Welcome session" });
-            ls.Add(new Programms { Duration = 45, Title = "Welcome session" });
+            ls.Add(new Programms { Duration = 30, Title = "Welcome session 5" });
+            ls.Add(new Programms { Duration = 15, Title = "Welcome session 6" });
+            ls.Add(new Programms { Duration = 60, Title = "Welcome session 7" });
+            ls.Add(new Programms { Duration = 45, Title = "Welcome session 8" });
 
             var alctmrng = new FirstSession(new DateTime(2016, 1, 1, 9, 0, 0), 180)
                                         .AllocateSession(ls);
 
 
             var sumOffirst = alctmrng.Sum(g => g.Duration);
+
+            Console.WriteLine("Monring");
+            foreach (var item in alctmrng.OrderBy(g=>g.Time))
+            {
+                
+                Console.WriteLine(string.Format("Time is {0} and program {1} and Duration {2}",item.Time.ToString("HH:mm"),item.Title,item.Duration));
+            }
+
+            var second = new  FirstSession(new DateTime(2016, 1, 1, 1, 0, 0), 180)
+                                        .AllocateSession(ls.Except(alctmrng).ToList());
+
+
+
+            Console.WriteLine("noon");
+            foreach (var item in second.OrderBy(g => g.Time))
+            {
+
+                Console.WriteLine(string.Format("Time is {0} and program {1} and Duration {2}", item.Time.ToString("HH:mm"), item.Title, item.Duration));
+            }
+
+
+            Console.ReadLine();
         }
     }
 
@@ -61,11 +83,22 @@ namespace Factorypattern
             var orderSession = new List<Programms>();
             foreach (Programms pgm in programs)
             {
-                //orderSession.Sum(g => g.Duration) <= MaxDuration &&
-                if (orderSession.Sum(g => g.Duration) < MaxDuration)
+                if (orderSession.Sum(g => g.Duration) <= MaxDuration)
                 {
                     if ((orderSession.Sum(g => g.Duration)) + pgm.Duration <= MaxDuration)
+                    {
+                        if (!orderSession.Any())
+                            pgm.Time = StartTime;
+                        else
+                        {
+                            pgm.Time = orderSession.Last().Time;
+                            pgm.Time= pgm.Time.AddMinutes(orderSession.Last().Duration);
+                        }
+
+
                         orderSession.Add(pgm);
+
+                    }
                 }
             }
 
@@ -112,5 +145,6 @@ namespace Factorypattern
     {
         public int Duration { get; set; }
         public string Title { get; set; }
+        public DateTime Time { get; set; }
     }
 }
